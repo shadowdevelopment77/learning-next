f6354649c6607c3969b9689c3e9eecd141b03116
shadowdevelopment77 <adlydevelopment37@gmail.com>
2026-05-17 17:35:24 +0700

    make a model for completed todos

--- todo-list/types/index.ts
+++ todo-list/types/index.ts
@@ -20,6 +20,13 @@ export interface User {
     is_active: boolean
 }
 
+export interface Completed {
+    id: number
+    title: string
+    completed_at: Date | null
+    user_id: number | null
+}
+
 export interface ApiResponse<T> {
     data: T
     message: string
